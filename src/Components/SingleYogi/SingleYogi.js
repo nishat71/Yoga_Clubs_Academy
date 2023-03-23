b0def13c329ae97c4bf0a71a37a7ac44a69103c3
Nishat <nishatbd17@gmail.com>
2023-03-23 11:50:44 +0600

    Single page done

--- src/Components/SingleYogi/SingleYogi.js
+++ src/Components/SingleYogi/SingleYogi.js
@@ -12,7 +12,7 @@ const SingleYogi = ({ yogi, handleToCart }) => {
         <h2 className='yogi-title'>{title}</h2>
         <p className='yogi-desc'>{description}</p>
         <p className='card-details-time'>Time required: <span className='card-details-time-span'>{time}</span>s</p>
-        <button onClick={() => handleToCart(yogi)} className='btn addList-btn'>Add to list</button>
+        {/* <button onClick={() => handleToCart(yogi)} className='btn addList-btn'>Add to list</button> */}
       </div>
     </div>
   )
